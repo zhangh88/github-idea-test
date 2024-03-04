04b91f5e0837fc974eb7a1f41f418b2d9436ff95
zhangzhang <15931294045@163.com>
2024-03-04 21:06:45 +0800

    hello github commit second

--- src/test/java/HelloGithub.java
+++ src/test/java/HelloGithub.java
@@ -1,5 +1,7 @@
 public class HelloGithub {
     public static void main(String[] args) {
         System.out.println("hello github");
+        //增加代码-第二版本
+        System.out.println("add source");
     }
 }
